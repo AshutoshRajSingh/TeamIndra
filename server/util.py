a405ad416881116b7dd2f1c6519037b214a847e3
= <=>
2022-09-18 00:22:46 +0530

    add aggregate_last_n_day usage

--- server/util.py
+++ server/util.py
@@ -5,6 +5,9 @@ MONTH_LENGTH = 30
 def aggregate_last_month_usage(usages: np.ndarray):
     return np.sum(usages[-MONTH_LENGTH:])
 
+def aggregate_last_n_day_usage(usages: np.ndarray, n: int):
+    return np.sum(usages[-n:])
+
 # Contains mapping to state names that have a representational discrepancy between
 # the dataset and the cea api
 MANUAL_MAPPING = {
